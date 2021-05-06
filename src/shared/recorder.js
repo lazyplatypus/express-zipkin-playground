ca5ac1e089cd54f1f01129226b6a930e77db353b
Daniel Kim <dgkim@ucdavis.edu>
2021-05-06 13:12:14 -0700

    made changes

--- src/shared/recorder.js
+++ src/shared/recorder.js
@@ -1,15 +1,13 @@
 const { BatchRecorder, jsonEncoder } = require("zipkin");
 const { HttpLogger } = require("zipkin-transport-http");
-
 const ZIPKIN_ENDPOINT = process.env.ZIPKIN_ENDPOINT || "http://localhost:9411";
-
 const recorder = new BatchRecorder({
   logger: new HttpLogger({
-    endpoint: `${ZIPKIN_ENDPOINT}/api/v2/spans`,
+    // endpoint: `${ZIPKIN_ENDPOINT}/api/v2/spans`,
+    endpoint: `${ZIPKIN_ENDPOINT}`,
     jsonEncoder: jsonEncoder.JSON_V2,
   }),
 });
-
 module.exports = {
   recorder,
 };
